13f293dfe24c81848f6c69323bcce70de757ae59
lell <elliott@gatsby.ucl.ac.uk>
2013-08-02 11:36:53 +0100

    adds static array catenation function

--- src/libnp/util/Operation.java
+++ src/libnp/util/Operation.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Operation {
@@ -302,5 +303,19 @@ public class Operation {
 			first = false;
 		}
 		return array;
+	}
+	
+	public static <T> T[] concat(T[] first, T[]... rest) {
+		int totalLength = first.length;
+		for (T[] array : rest) {
+			totalLength += array.length;
+		}
+		T[] result = Arrays.copyOf(first, totalLength);
+		int offset = first.length;
+		for (T[] array : rest) {
+			System.arraycopy(array, 0, result, offset, array.length);
+			offset += array.length;
+		}
+		return result;
 	}
 }
